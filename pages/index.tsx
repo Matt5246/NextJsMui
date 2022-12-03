90e0f2a72332274b34d862ae175f7c646a6f551c
Matt5246 <116891485+Matt5246@users.noreply.github.com>
2022-12-03 22:07:22 +0100

    new file:   pages/blog.tsx 	modified:   pages/header.tsx 	modified:   pages/index.tsx 	new file:   pages/pricing.tsx 	new file:   pages/products.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,14 @@ import styles from "../styles/Home.module.css";
 import Switch from "@mui/material/Switch";
 // import Header from './header'
 import Content from './content';
+import Header from './header';
 const label = { inputProps: { "aria-label": "Switch demo" } };
 
 export default function Home() {
   const [switchDisable, setSwitchDisable] = React.useState(false);
  return (
   <>
+    <Header/>
     <Content/>
   </>
    
